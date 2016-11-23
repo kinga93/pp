b7dc203e6acd9c80f46356fe1c91f7fbf6f6edcb
Kinga Wojs <kinga.wojs93@gmail.com>
2016-11-23 17:57:04 +0100

    zajecia 2, zapis pracy

---
+++ zajecia2/zad1/z1/zad1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int a=0;
+  int b=0;
+
+   cout<<"Wprowadz przedzial liczb od: ";
+   cin>>a;
+
+   cout<<"do: ";
+   cin>>b;
+
+
+for (int i=a;i<b;i++){
+
+cout<<i; 
+
+}
+
+   return 0;
+}
